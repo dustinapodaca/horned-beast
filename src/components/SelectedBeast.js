d36809af4c67b4d6975693a868988ae7cb64d07c
dustinapodaca <dustinapodaca@gmail.com>
2022-09-22 16:45:36 -0600

    finished modal

--- src/components/SelectedBeast.js
+++ src/components/SelectedBeast.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class SelectedBeast extends React.Component {
@@ -8,20 +9,22 @@ class SelectedBeast extends React.Component {
     console.log(this.props);
     return (
       <>
-        <Modal show={this.props.showModal} onHide={this.props.handleClose} >
-          <Modal.Header closeButton>
-            <Modal.Title>{this.props.displayBeast.title}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <img src={this.props.displayBeast.image_url} alt={this.props.displayBeast.title} className="w-75"/>
-            <p>{this.props.displayBeast.description}</p>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={this.props.handleClose}>
-              Close
-            </Button>
-          </Modal.Footer>
-        </Modal>
+        <Container>
+          <Modal show={this.props.showModal} onHide={this.props.handleClose} >
+            <Modal.Header closeButton>
+              <Modal.Title>{this.props.displayBeast.title}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <img src={this.props.displayBeast.image_url} alt={this.props.displayBeast.title} className="w-75"/>
+              <p>{this.props.displayBeast.description}</p>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={this.props.handleClose}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </Container>
       </>
     );
   }
