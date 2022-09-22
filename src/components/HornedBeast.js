1ceff6233bc420fbfab93da2efc3a687ad576b62
dustinapodaca <dustinapodaca@gmail.com>
2022-09-22 16:39:11 -0600

    finished modal

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -4,11 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
-class Beasts extends React.Component {
+class HornedBeast extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       _id: this.props._id,
+      key: this.props._id,
       image_url: this.props.image_url,
       title: this.props.title,
       description: this.props.description,
@@ -27,7 +28,8 @@ class Beasts extends React.Component {
   render() {
     return (
       <Card style={{ width: '18rem' }} className="h-100 mx-auto px-3">
-        <Card.Img className="py-4" variant="top" src={this.state.image_url} alt={this.state.keyword} title={this.state.title} onClick={this.props.handleShowBeast} />
+        <Card.Img className="py-4" variant="top" src={this.state.image_url} alt={this.state.keyword} title={this.state.title} onClick={() => {
+          this.props.handleShowBeast(this.props.id)}} />
         <Card.Body className="py-auto">
           <Card.Title>{this.state.title}</Card.Title>
           <Card.Text>{this.state.description}</Card.Text>
@@ -40,19 +42,4 @@ class Beasts extends React.Component {
   }
 }
 
-export default Beasts;
-
-
-
-
-// render() {
-//   return (
-//     <>
-//       <div class="beasts">
-//         <h2>{this.props.title}</h2>
-//         <img id="imgBeasts" src={this.props.src} alt={this.props.alt} title={this.props.title} />
-//         <p>{this.props.description}</p>
-//       </div>
-//     </>
-//   );
-// }
+export default HornedBeast;
